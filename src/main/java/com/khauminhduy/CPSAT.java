5661e3d7cdcedfc1c9312760db8824fa025d8400
khauminhduy <khauminhduy@gmail.com>
2021-07-04 22:19:41 +0700

    update

--- src/main/java/com/khauminhduy/CPSAT.java
+++ src/main/java/com/khauminhduy/CPSAT.java
@@ -11,51 +11,52 @@ public class CPSAT {
 
 	public static void main(String[] args) {
 		Loader.loadNativeLibraries();
-		int[][] costs = { 
-				{ 18, 52, 64, 39 },
-				{ 75, 55, 19, 48 },
-				{ 35, 57, 8, 65 },
-				{ 27, 25, 14, 16 }
+		
+		String[] workers = {"A", "B", "C", "D"};
+		
+		int[] costs = { 
+				 18, 52, 64, 39 ,
+				 75, 55, 19, 48 ,
+				 35, 57, 8, 65 ,
+				 27, 25, 14, 16 
 			};
 		
 		CpModel model = new CpModel();
-		final int numWorkers = costs.length;
-		final int numTasks = costs[0].length;
+		final int numWorkers = workers.length;
+		final int numTasks = 4;
 		
 		IntVar[][] variables = new IntVar[numWorkers][numTasks];
 		IntVar[] variablesFlat = new IntVar[numWorkers * numTasks];
 		
-		int[] costsFlat = new int[numWorkers * numTasks];
-		
 		for(int i = 0; i < numWorkers; i++) {
 			for(int j = 0; j < numTasks; j++) {
-				// 0 <= x <= 1
 				variables[i][j] = model.newIntVar(0, 1, "");
 				int k = i * numTasks + j;
 				variablesFlat[k] = variables[i][j];
-				costsFlat[k] = costs[i][j];
 			}
 		}
 		
 		for(int i = 0; i < numWorkers; i++) {
 			IntVar[] vars = new IntVar[numTasks];
+			IntVar[] vars_column = new IntVar[numWorkers];
 			for(int j = 0; j < numTasks; j++) {
 				vars[j] = variables[i][j];
+				vars_column[j] = variables[j][i];
 			}
-			// tong cua dong <= 1
 			model.addLessOrEqual(LinearExpr.sum(vars), 1);
+			model.addEquality(LinearExpr.sum(vars_column), 1);
 		}
 		
-		for(int j = 0; j < numTasks; j++) {
-			IntVar[] vars = new IntVar[numWorkers];
-			for(int i = 0; i < numWorkers; i++) {
-				vars[i] = variables[i][j];
-			}
-			// tong cua cot = 1
-			model.addEquality(LinearExpr.sum(vars), 1);
-		}
+//		for(int j = 0; j < numTasks; j++) {
+//			IntVar[] vars = new IntVar[numWorkers];
+//			for(int i = 0; i < numWorkers; i++) {
+//				vars[i] = variables[i][j];
+//			}
+//			// tong cua cot = 1
+//			model.addEquality(LinearExpr.sum(vars), 1);
+//		}
 		
-		model.minimize(LinearExpr.scalProd(variablesFlat, costsFlat));
+		model.minimize(LinearExpr.scalProd(variablesFlat, costs));
 		CpSolver solver = new CpSolver();
 		CpSolverStatus status = solver.solve(model);
 		if(status == CpSolverStatus.OPTIMAL) {
@@ -63,7 +64,7 @@ public class CPSAT {
 			for(int i = 0; i < numWorkers; i++) {
 				for(int j = 0; j < numTasks; j++) {
 					if(solver.value(variables[i][j]) == 1) {
-						System.out.println("Worker " + i + " assigned to task " + j + ".  Cost = " + costs[i][j]);
+						System.out.println("Worker " + workers[i] + " assigned to task " + (j+1) + ".  Cost = " + costs[i * numTasks + j]);
 					}
 				}
 			}
